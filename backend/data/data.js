fd1d53b3abefe3b3648b0c3577eb55f4639dfaca
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-17 12:04:30 -0400

    babel installed

---
+++ backend/data/data.js
@@ -0,0 +1,19 @@
+export default {
+    students: [
+        {
+            id: '1',
+            firstName: 'Jhon',
+            lastName: 'Doe'
+        },
+        {
+            id: '2',
+            firstName: 'Jane',
+            lastName: 'foo'
+        },
+        {
+            id: '3',
+            firstName: 'Mark',
+            lastName: 'buz'
+        }
+    ]
+}
---
+++ backend/server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import data from './data/data';
+
+const app = express();
+const port = 5000;
