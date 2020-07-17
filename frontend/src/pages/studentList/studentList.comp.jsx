accd11bd734adadf0422a46701779dabbe56412a
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-17 09:12:20 -0400

    pages created

---
+++ frontend/src/pages/studentList/studentList.comp.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function StudentListPage(props) {
+    return (
+        <div className="student-list">
+            <div className="list-container">
+                <div className="list">
+                    <ul>
+                        <li>
+                            Student 1
+                        </li>
+                        <li>
+                            Student 2
+                        </li>
+                        <li>
+                            Student 3
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StudentListPage;
